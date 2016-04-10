49424f27519f631a213b8f1f502c07fa3469376d
Marc Cooperstein <mcooperstein@gmail.com>
2016-04-10 16:28:48 -0700

    added pipes
    -added pipes
    -added gap between pipes

--- js/components/graphics/pipe.js
+++ js/components/graphics/pipe.js
@@ -6,6 +6,15 @@ var PipeGraphicsComponent = function (entity) {
 
 PipeGraphicsComponent.prototype.draw = function (context) {
     //console.log("Drawing a Pipe");
+    var position = this.entity.components.physics.position;
+
+    context.save();
+    context.translate(position.x, position.y);
+    context.beginPath();
+    //context.fill(0, 0, this.entity.size.width, this.entity.size.height);
+    context.fillRect(0, 0, 0.1, 1);
+    context.closePath();
+    context.restore();
 };
 
 //exports.PipeGraphicsComponent = PipeGraphicsComponent;
--- js/entities/pipe.js
+++ js/entities/pipe.js
@@ -1,14 +1,19 @@
 'use strict';
 
 var PipeGraphicsComponent = require("../components/graphics/pipe");
+var PhysicsComponent = require("../components/physics/physics");
 
-var Pipe = function () {
-    console.log("Creating Pipe entity");
+var Pipe = function (position) {
+    //console.log("Creating Pipe entity");
 
     //var graphics = new graphicsComponent.PipeGraphicsComponent(this);
+    //var physics = new physicsComponent.PhysicsComponent(this);
     this.components = {
-        graphics: new PipeGraphicsComponent(this)
+        graphics: new PipeGraphicsComponent(this),
+        physics: new PhysicsComponent(this)
     };
+    this.components.physics.position.x = position.x;
+    this.components.physics.position.y = position.y;
 };
 
 module.exports = Pipe;
